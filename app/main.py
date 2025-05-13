e7fd18732b88317b1a65feaa6d2f19b9c6ac20e4
daniel <d.morgan.biddick@gmail.com>
2025-05-13 03:39:12 +0100

    maybe fixed

--- app/main.py
+++ app/main.py
@@ -36,4 +36,4 @@ async def websocket_endpoint(websocket: WebSocket):
         clients.remove(websocket)
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080)
